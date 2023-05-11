092df80de293bcd41db4b94b8267b935c7c4e5e6
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2023-05-11 11:32:21 +0300

    hookform and asyncstorage

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStack } from "./stack/MainStack";
 import { AuthStack } from "./stack/AuthStack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const Navigator = () => {
+    let isAuth = false;
 
-  const isAuth = false;
+    const checkToken = async () => {
+      try {
+        const value = await AsyncStorage.getItem("@token");
+        if (value !== null) {
+          isAuth = true;
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
-  return (
-    <NavigationContainer>
-      {isAuth ? <MainStack /> : <AuthStack />}
+    useEffect(() => {
+      checkToken();
+    }, []);
 
-    </NavigationContainer>
-  );
-};
+    return (
+      <NavigationContainer>
+        {isAuth ? <MainStack /> : <AuthStack />}
+
+      </NavigationContainer>
+    );
+  }
+;
--- src/screens/auth/AuthScreen.tsx
+++ src/screens/auth/AuthScreen.tsx
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { AgEnum, Text } from "../../components/ui/Text";
 import { Input } from "../../components/ui/Input";
 import { Colors } from "../../styles/Colors";
+import { Button } from "../../components/ui/Button";
+import { useForm } from "react-hook-form";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+enum EAuthForm {
+  USERNAME = "username",
+  PASSWORD = "password"
+}
 
 export const AuthScreen = () => {
+  const { watch, setValue, register, formState: { errors }, handleSubmit } = useForm();
+
+  const handleChange = (value: string, name: string) => {
+    setValue(name, value);
+  };
+
+  const saveUserToken = async (token: string) => {
+    try {
+      console.log('Save 123')
+      await AsyncStorage.setItem("@token", token);
+    } catch (e) {
+      console.log('Error', e)
+    }
+  };
+
+  const sendData = (data: any) => {
+    if (data.password.length > 5) {
+      saveUserToken("TOKENASDASD");
+    }
+  };
+
+  useEffect(() => {
+    register(EAuthForm.USERNAME, { required: true });
+    register(EAuthForm.PASSWORD, { required: true, minLength: 6 });
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text align={"center"} style={styles.title} Ag={AgEnum.H1}>
         Instagram
       </Text>
-      <View style={{marginTop: 32}}>
-        <Input inputKey={''} error={''} placeholder={'username'}></Input>
-        <Input inputKey={''} error={''} placeholder={'password'} marginTop={0}></Input>
+      <View style={{ marginTop: 32 }}>
+        <Input value={watch(EAuthForm.USERNAME)}
+               inputKey={EAuthForm.USERNAME}
+               onChangeText={handleChange}
+               error={""}
+               placeholder={"username"}></Input>
+        <Input value={watch(EAuthForm.PASSWORD)}
+               inputKey={EAuthForm.PASSWORD}
+               onChangeText={handleChange}
+               error={""}
+               placeholder={"password"}
+               marginTop={0}></Input>
+
+        <Button title={"Login"} onPress={
+          handleSubmit(sendData)} />
       </View>
     </View>
   );
